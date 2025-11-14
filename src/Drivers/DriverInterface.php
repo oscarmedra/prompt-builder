a7f313646104537837802e8266ef14f7075ef368
noah-medra <noudrahame97@gmail.com>
2025-11-14 09:11:38 +0000

    Update DriverInterface.php

--- src/Drivers/DriverInterface.php
+++ src/Drivers/DriverInterface.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Facade;
 use NoahMedra\PromptBuilder\BuilderInput;
 use NoahMedra\PromptBuilder\BuilderOutput;
 
-Facade::setFacadeApplication($app);
-
-
 interface DriverInterface
 {
     public function process(BuilderInput $input) : BuilderOutput;
